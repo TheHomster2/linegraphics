79507296cb4adaeda7f098c89cc35612440f23cb
Anthony Hom <anthonyhom2@hotmail.com>
2018-02-11 23:20:10 -0500

    Done

---
+++ draw.c
@@ -0,0 +1,106 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+#include "ml6.h"
+#include "display.h"
+#include "draw.h"
+
+int octant(int x0, int y0, int x0, int x1){
+	int deltax = x1 - x0;
+	int deltay = y1 - y0;
+
+	//octant 1 with bound 0 to 1
+	if (deltay >= 0 && deltax >= deltay){
+		return 1;
+	}
+	//octant 2 could tend to 1
+	else if (deltay >= 0 && deltax <= deltay){
+		return 2;
+	}
+	//octant 7 with bound of -1 to 0
+	else if (deltay < 0 && deltax <= -1 * deltay){
+		return 7;
+	}
+	//octant 8 could tend to -1
+	else if (deltay < 0 && deltax >= -1 * deltay){
+		return 8;
+	}
+	//error
+	else {
+
+	return -1;
+	
+	}
+}
+
+//Insert your line algorithm here
+void draw_line(int x0, int y0, int x1, int y1, screen s, color c) {
+	if (x1 < x0){
+		int temp = x0;
+		x0 = x1;
+		x1 = temp;	
+		temp = y0;
+		y1 = y0;
+		y1 = temp;
+	}
+
+	int A = y1 - y0;
+	int B = x0 - x1;
+	int d;
+	int add_octant = octant(x0, y0, x1, y1);
+	int x = x0;
+	int y = y0;
+
+	if (add_octant = 1){
+		d = 2 * A + B;
+		while (x <= x1){
+			plot(s, c, x, y);
+			if (d > 0){
+				y++;
+				d += 2 * B;
+			}
+			x++;
+			d += 2 * A;
+		}
+	}
+
+	else if (add_octant = 2){
+		d = A + 2 * B;
+		while (y <= y1){
+			plot(s, c, x, y);
+			if (d < 0){
+				x++;
+				d += 2 * A;
+			}
+			y++;
+			d += 2 * B;
+		}
+	}
+
+	else if (add_octant = 7){
+		d = A - 2 * B;
+		while (y >= y1){
+			plot(s, c, x, y);
+			if (d > 0){
+				x++;
+				d += 2 * A;
+			}
+			y--;
+			d -= 2 * B;
+		}
+	}
+
+	else{
+
+		d = 2 * A - B;
+		while (x <= x1){
+			plot(s, c, x, y);
+			if (d < 0){
+				y--;
+				d -= 2 * B;
+			}
+			x++;
+			d += 2 * B;
+		}
+	}
+}
---
+++ main.c
@@ -0,0 +1,57 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+#include "ml6.h"
+#include "display.h"
+#include "draw.h"
+
+int main() {
+
+  screen s;
+  color c;
+
+  c.red = 130;
+  c.green = 150;
+  c.blue = MAX_COLOR;
+
+  clear_screen(s);
+
+  int x0 = 250;
+  int y0 = 326;
+  int x1 = 0;
+  int y2 = -132;
+
+  for (int i = 0; i < 20; i++){
+  	c.red = 12;
+  	c.blue = MAX_COLOR / 2;
+  	c.green = 200;
+  	x0 -= 13;
+  	y0 += 79;
+  	x1 += 40;
+  	y1 -= 37;
+  	draw_line(x0, y0, x1, y1, s, c);
+  }
+
+  for (int i = 50; i < 250; i+= 20){
+  	c.red = MAX_COLOR;
+  	c.blue = 140;
+  	c.green = MAX_COLOR / 4;
+  	draw_line(0, 500, i, i , s, c);
+  	draw_line(500, 0, -i, -i, s, c);
+  	draw_line(i, -i, 0, 500, s, c);
+  	draw_line(-i, i, 500, 0, s, c);
+  }
+
+  c.red = MAX_COLOR / 2;
+  c.green = MAX_COLOR / 3;
+  c.blue = MAX_COLOR / 4;
+
+  draw_line(0, 500, 500, 0, s, c);
+  draw_line(0, 0, 500, 500, s, c);
+  draw_line(0, 250, 500, 250, s, c);
+  draw_line(250, 0, 250, 500, s, c);
+
+  display(s);
+  save_extension(s, "lines.png");
+}
